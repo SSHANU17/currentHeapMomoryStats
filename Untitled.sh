ba6d9b55b045b99f3f6eec650c9926ff5eb68c2f
Pranav Mittal <33569604+SSHANU17@users.noreply.github.com>
2023-06-23 12:44:20 +0530

    Update Untitled.sh

--- Untitled.sh
+++ Untitled.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-# Script to get pods for Kubernetes/Docker and to calculate the currentHeapMomory, PID and Monitor it
 start_time=$(date +%s)
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-filenameSuffix=$(date "+%Y-%m-%d_%H_%M_%S")
+filenameSuffix=$(date "+%Y_%m_%d__%H_%M_%S")
 command="kubectl get po -n redis"
 command2="kubectl get nodes"
 interval=30
-# time interval to repeat in seconds
 log_file="LoadMonitoringLog_$filenameSuffix.log"
-# Log file to record the output 
-declare -a apps=("ABC" "BCD" "CDE")
-# PODS name
-namespace= NAMESAPCE
-# NAMESAPCE :- replace it with yours
+declare -a apps=("ABC" "BCD" "CDE" "DEF" "EFG")
+namespace=YOURNAMESPACE
 read -p "Enter the duration in seconds: " duration
+
+podNames=()
+PIDS=()
+echo "Fetching podnames and pids. Please wait..."
+for appname in "${apps[@]}"
+do
+    podname=$(kubectl get pods -o name --selector=app=$appname -n $namespace | awk '{split($0,a,"/"); print a[2]}')
+    podNames+=("$podname")
+    PID=$(kubectl exec -it $podname -n $namespace -- jps | awk 'NR==1{print $1}')
+    PIDS+=("$PID")
+done
 
 while true; do
     output=$(eval "$command")
@@ -31,15 +37,14 @@ while true; do
     echo -e "[$timestamp] Output: \n$output2"
     echo -e "=============================================================================================================" >> "$log_file"
     echo -e "============================================================================================================="
-
-    for appname in "${apps[@]}"
+    for ((i=0; i<${#podNames[@]}; i++));
     do
-        podname=$(kubectl get pods -o name --selector=app=$appname -n $namespace | awk '{split($0,a,"/"); print a[2]}')
-        echo "Current occupied heap space in pod: " $podname
-        echo "Current occupied heap space in pod: " $podname >> "$log_file"
-        PID=$(kubectl exec -it $podname -n $namespace -- jps | awk 'NR==1{print $1}')
+        # podname=$(kubectl get pods -o name --selector=app=$appname -n $namespace | awk '{split($0,a,"/"); print a[2]}')
+        echo "Current occupied heap space in pod: " ${podNames[i]}
+        echo "Current occupied heap space in pod: " ${podNames[i]} >> "$log_file"
+        # PID=$(kubectl exec -it $podname -n $namespace -- jps | awk 'NR==1{print $1}')
         # command3=""
-        output3=$(kubectl exec -it $podname -n $namespace -- jstat -gc $PID 2>/dev/null | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; print sum/1024}' 2>/dev/null)
+        output3=$(kubectl exec -it "${podNames[i]}" -n $namespace -- jstat -gc "${PIDS[i]}" 2>/dev/null | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; print sum/1024}' 2>/dev/null)
         # echo -e "[$timestamp] Command: $command3" >> "$log_file"
         # echo -e "[$timestamp] Command: $command3"
         echo -e "[$timestamp] Occupied heap space: \n$output3 Mb" >> "$log_file"
@@ -47,7 +52,6 @@ while true; do
         echo -e "=============================================================================================================" >> "$log_file"
         echo -e "============================================================================================================="
     done
-
     current_time=$(date +%s)
     elapsed_time=$((current_time - start_time))
     if [[ $elapsed_time -ge $duration ]]; then
